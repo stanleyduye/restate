3b0283ef19b46dbaf018cfa021050b1472b7ffde
priyed <stanleyduye@gmail.com>
2025-07-28 11:23:50 +0100

    initialize app

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Text className="text-2xl">Welcome to Estates</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {SplashScreen, Stack} from "expo-router";
+import "./global.css"
+import React, {useEffect} from "react";
+import {useFonts} from "expo-font";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const [fontsLoaded] = useFonts({
+        "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+        "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+        "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+        "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+        "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+        "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+    })
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    if (!fontsLoaded) return null;
+  return <Stack />;
 }
